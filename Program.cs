7940a2fa620818dbbe84875b75923dbb25ddd095
Anup Shetty <anupshetty33@gmail.com>
2024-10-18 14:43:47 +0530

    ag - added consul service provider

--- Program.cs
+++ Program.cs
@@ -1,17 +1,18 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
 
 var builder = WebApplication.CreateBuilder(args);
-var appUrl = builder.Configuration["React-App:Url"];
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
 
-// Add Ocelot services to the container
-builder.Services.AddOcelot(builder.Configuration);
-builder.Logging.AddConsole();
+var appUrl = builder.Configuration["React-App:Url"];
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOcelot(builder.Configuration).AddConsul();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowReactApp",
@@ -19,17 +20,17 @@ builder.Services.AddCors(options =>
                           .AllowAnyHeader()
                           .AllowAnyMethod());
 });
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-//app.UseHttpsRedirection();
 app.UseCors("AllowReactApp");
+
 await app.UseOcelot();
 
 app.Run();
